ed606aed8500d3a3ba36d52bf90651b9b74dd1b5
Dean Macaulay <dean@magpies.net>
2016-01-15 14:48:45 +1100

    Initial Commit

---
+++ RSSFeedReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSFeedReader
+//
+//  Created by Dean on 15/01/2016.
+//  Copyright © 2016 Macaulay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
